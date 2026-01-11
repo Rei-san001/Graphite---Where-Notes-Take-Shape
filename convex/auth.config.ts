e640e894464ef119305d1ecff20fe5a2ba9e894a
Nahid Bin Islam <179292160+Rei-san001@users.noreply.github.com>
2026-01-11 19:28:06 +0600

    update

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,8 +3,8 @@ import { AuthConfig } from "convex/server";
 export default {
   providers: [
     {
-      domain: "https://tolerant-martin-13.clerk.accounts.dev",
-      applicationID: "convex",
+      //...
+      //...
     },
   ]
 } satisfies AuthConfig;
